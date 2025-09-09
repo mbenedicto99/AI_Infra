52883aa13367ab62f12c72db96027207eb5bc665
mbenedicto99 <mbenedicto@gmail.com>
2025-09-09 19:53:14 -0300

    initial

--- 1.sh
+++ 1.sh
@@ -2,9 +2,9 @@
 git remote -v
 
 # corrigir a URL (remova a barra final e confirme o owner)
-git remote set-url origin https://github.com/mbenedicto99/AI_Infra.git
+#git remote set-url origin https://github.com/mbenedicto99/AI_Infra.git
 # se preferir SSH (evita PAT):
-# git remote set-url origin git@github.com:mbenedicto99/AI_Infra.git
+git remote set-url origin git@github.com:mbenedicto99/AI_Infra.git
 
 git add -A
 git commit -m "initial"  # se ainda não tiver commit
